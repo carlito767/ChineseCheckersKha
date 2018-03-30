6ba0a6e9b7e6b4fdc5d27d7d35026dc82a67d488
carlito <charles.longuemart@gmail.com>
2018-03-30 02:28:09 +0200

    HTML5 target (resizable canvas)

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -9,6 +9,7 @@
 #if kha_html5
 import js.html.CanvasElement;
 import js.Browser.document;
+import js.Browser.window;
 #end
 import kha.System;
 
@@ -20,6 +21,18 @@ class Main {
   static public var DEVMODE = false;
 
   static public function main() {
+    #if kha_html5
+    var resize = function() {
+      var canvas = cast(document.getElementById('khanvas'), CanvasElement);
+      canvas.width = Std.int(window.innerWidth * window.devicePixelRatio);
+      canvas.height = Std.int(window.innerHeight * window.devicePixelRatio);
+      canvas.style.width = document.documentElement.clientWidth + 'px';
+      canvas.style.height = document.documentElement.clientHeight + 'px';
+    }
+    window.onresize = resize;
+    resize();
+    #end
+
     System.init({ title:Game.TITLE, width:Game.WIDTH, height:Game.HEIGHT, samplesPerPixel:4 }, Loader.init);
   }
 }
